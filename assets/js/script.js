68cd05d0c57edbbd140f6661fb6d7b80d01aa824
macchiagabriele13 <macchia_gabriele@libero.it>
2022-10-17 15:14:54 +0200

    commit - grid

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,34 @@
 /* L'utente clicca su un bottone che genererà una griglia di gioco quadrata. */
 
+/* Seleziono il bottone */
+const bottoneGeneratore = document.querySelector(".btn_play")
+console.log(bottoneGeneratore);
+
+/* Seleziono la griglia */
+const containerGrid = document.querySelector('.container')
+console.log(containerGrid);
+
+/* Creo i miei box */
+
+for (let i = 1; i <= 100; i++) {
+  const boxEl = document.createElement('div')
+  boxEl.classList.add('box')
+  containerGrid.append(boxEl)
+}
+  /* //console.log(boxEl); */
+
+
+
+
+
+/* Al click compare la mia griglia */
+
+
+/* bottoneGeneratore.addEventListener("click", function name(params) {
+    document.getElementByClass(".container").style.display = "none";
+});
+ */
+
 
 /* Ogni cella ha un numero progressivo, da 1 a 100. */
 
